5888da6d9f71aa70703e6b5bc51a3e197efd5b37
AYC <allenyoungchina@outlook.com>
2024-06-18 17:10:22 +0800

    项目初始化

---
+++ todo/__init__.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    """工厂函数"""
+    # 创建并配置应用
+    # instance_relative_config=True方便后续从instance目录读取配置文件
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'todo.sqlite'),
+    )
+
+    if test_config is None:
+        # 从instance目录加载配置文件
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # 加载测试配置test_config
+        app.config.from_mapping(test_config)
+
+    # 确保实例instance目录存在
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # 测试路由
+    @app.route('/hello')
+    def hello_world():
+        return 'Hello, World!'
+
+    # 注册数据库相关操作
+    from . import db
+    db.init_app(app)
+
+    # 注册认证相关蓝图
+
+    # 注册待办相关蓝图
+
+    return app
---
+++ todo/db.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+import click
+
+from flask import current_app, g
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        # 指定连接像字典一样返回的行数据，可以通过字段名获取字段值
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    """数据库初始化操作，清除现有数据，并创建表格"""
+    db = get_db()
+
+    with current_app.open_resource('schema.sql', mode='rb') as f:
+        db.executescript(f.read().decode('utf-8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    """数据库初始化命令"""
+    init_db()
+    click.echo('数据库初始化已完成。')
+
+
+def init_app(app):
+    """用于将数据库相关操作注册到应用上"""
+    # 请求处理完毕，响应返回之前关闭数据库连接
+    app.teardown_appcontext(close_db)
+    # 注册执行数据库初始化操作的命令行命令
+    app.cli.add_command(init_db_command)
